e1d5f997d1ce84b04b611a76ab1da67bf649d4ec
daw2-millan22 <millanfernandezricardo@fpllefia.com>
2022-01-17 11:16:43 +0100

    commit de simplificacion de vistas

---
+++ webapp/controller/View1.controler.js
@@ -0,0 +1,36 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+
+    function (Controller, JSONModel) {
+        "use strict";
+
+        return Controller.extend("tutorial1.controller.View1", {
+            onInit: function () {
+                this.getOwnerComponent().setModel(new JSONModel("../model/cities.json"), "CitiesModel")
+            },
+            
+            onSayHello: function(oEvent) {
+                /*
+
+                MessageToast.show("Hola Ricardo", {
+                    duration: 3000,
+                });
+                */
+               if(!this._dialogSayHello){
+                   this._dialogSayHello = sap.ui.xmlfragment("idSayHelloDialog", "tutorial1.controller.View1");
+                   this.getView().addDependent(this._dialogSayHello);
+               }
+               this._dialogSayHello.open();
+            },
+            onClose: function (){
+                if(this._dialogSayHello){
+                    this._dialogSayHello.close();
+                }
+            }
+        });
+    });
